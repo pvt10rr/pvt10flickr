3580ab2231b1abe9d58bd49e5d7d5dcc227c771c
George-N <geo.niescieronek@gmail.com>
2011-04-07 11:22:40 +0200

    testing new adds

---
+++ RapidReporterFlickrAddon_V0008/FlickrAddon.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FlickrNet;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Rapid_Reporter
+{
+    class FlickrAddon
+    {            
+        private Flickr m_flickr;
+        private string m_frob;
+        private string m_token;
+        private Auth m_auth;
+
+        private string m_tags;
+
+        private string API_KEY = "d06d4e9bb977706b443f5cb969b29a94";
+        private string SECRET = "a62d4e4bed745bf0";
+
+
+
+        public void Login()
+        {
+
+            // Create Flickr instance
+            m_flickr = new Flickr(API_KEY, SECRET);
+
+            m_frob = m_flickr.AuthGetFrob();
+
+            string flickrUrl = m_flickr.AuthCalcUrl(m_frob, AuthLevel.Write);
+
+            // The following line will load the URL in the users default browser.
+            System.Diagnostics.Process.Start(flickrUrl);
+
+            bool bIsAuthorized = false;
+            m_auth = new Auth();
+
+            // do nothing until flickr authorizes.
+            while (!bIsAuthorized)
+            {
+                try
+                {
+                    m_auth = m_flickr.AuthGetToken(m_frob);
+                    m_flickr.AuthToken = m_auth.Token;
+                }
+                catch (FlickrException ex)
+                {
+                    ;
+                }
+
+                if (m_flickr.IsAuthenticated)
+                {
+                    bIsAuthorized = true;
+                }
+            }
+        }
+
+        public string GetCurrentUser()
+        {
+                return m_auth.User.UserName; 
+ 
+        }
+
+
+        public void Upload(string pFile, string pName, string pDesc, string pTags)
+        {
+
+            if (SMWidget.ToggleUpload2 == true)
+            {
+                try
+                {
+                    bool b = m_flickr.IsAuthenticated;
+                }
+                catch (FlickrException f)
+                {
+                    MessageBox.Show(f.Message);
+
+                }
+
+                if (IsAuthenticated())
+                {
+                    m_flickr.UploadPicture(pFile, pName, pDesc, pTags);
+                }
+                else
+                {
+                    Login();
+                    m_flickr.UploadPicture(pFile, pName, pDesc, pTags);
+                }
+            }
+        }
+
+        public bool IsAuthenticated()
+        {
+            return m_flickr.IsAuthenticated;
+        }
+
+
+        public void LogOut()
+        {
+            m_flickr.AuthToken = null;
+            //string url = m_flickr.AuthCalcWebUrl(AuthLevel.Write);
+            System.Diagnostics.Process.Start("http://www.flickr.com/logout.gne?");
+        }
+    }
+
+    
+
+
+
+    
+}
+
+
